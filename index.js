73b56380cd09749929d481517cb769311bb33226
hellshade <midnight3animal@gmail.com>
2025-09-07 23:45:22 +0500

    feat: реализовать динамическую видимость кнопок скролла строки карточек

--- index.js
+++ index.js
@@ -19,4 +19,33 @@ const setupCardScroller = () => {
     });
 };
 
+const UpdateButtonVisibility = () => {
+    const cardsRow = document.querySelector(".cards-row");
+    const scrollForwardButton = document.querySelector(".scroll-forward-button");
+    const scrollBackButton = document.querySelector(".scroll-back-button");
+
+    const updateButtonsState = () => {
+        const currentPosition = cardsRow.scrollLeft;
+        const cardsRowWidth = cardsRow.scrollWidth;
+        const cardsVisibleWidth = cardsRow.clientWidth;
+
+        if (currentPosition === 0) {
+            scrollBackButton.classList.add("hidden");
+        } else {
+            scrollBackButton.classList.remove("hidden");
+        }
+        
+        if (currentPosition + cardsVisibleWidth === cardsRowWidth) {
+            scrollForwardButton.classList.add("hidden");
+        } else {
+            scrollForwardButton.classList.remove("hidden");
+        }
+
+    };
+
+    cardsRow.addEventListener("scroll", updateButtonsState);
+    updateButtonsState();
+}
+
+UpdateButtonVisibility();
 setupCardScroller();
