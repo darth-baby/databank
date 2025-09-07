7ceadc730e59be9402a8f51c17c65182b02ca33f
hellshade <midnight3animal@gmail.com>
2025-09-07 20:51:30 +0500

    feat: Реализовал функционал кнопок прокрутки влево-вправо и добавил описание в README.md

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const scrollCardsRow = () => {
-    const cardsRow = document.querySelector(".cards-row")
-    const scrollNextButton = document.querySelector(".scroll-forward-button");
-    const cardsRowWidth = cardsRow.clientWidth;
-    
+const setupCardScroller = () => {
+    const cardsRow = document.querySelector(".cards-row");
+    const scrollForwardButton = document.querySelector(".scroll-forward-button");
+    const scrollBackButton = document.querySelector(".scroll-back-button");
+    const scrollAmount = cardsRow.clientWidth;
 
-    scrollNextButton.addEventListener("click", () => {
+    scrollForwardButton.addEventListener("click", () => {
         cardsRow.scrollBy({
-            left: cardsRowWidth,
+            left: scrollAmount,
             behavior: 'smooth'
-        })
-    })
+        });
+    });
 
+    scrollBackButton.addEventListener("click", () => {
+        cardsRow.scrollBy({
+            left: -scrollAmount,
+            behavior: "smooth"
+        });
+    });
 };
 
-scrollCardsRow();
+setupCardScroller();
